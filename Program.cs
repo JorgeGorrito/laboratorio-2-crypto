58b0143022d29cc497bd55174e9358260457c567
JAAbella <jorgeabella@lavenir.com.co>
2024-04-06 04:47:08 -0500

    Se configuro el menu  de la aplicacion para su funcionamiento

--- Program.cs
+++ Program.cs
@@ -6,56 +6,10 @@ class Program
 {
     static void Main()
     {
-        try
-        {
-            // Crear una instancia de la clase RSACryptoServiceProvider
-            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
-            {
-                // Generar el par de claves pública y privada
-                RSAParameters privateKey = rsa.ExportParameters(true);
-                RSAParameters publicKey = rsa.ExportParameters(false);
-
-
-                // Guardar la clave pública en un archivo.
-                File.WriteAllText("publicKey.txt", ToXmlString(publicKey));
-
-                // Firmar el archivo de texto
-                string mensaje = "Hola, mundo!";
-                byte[] mensajeBytes = System.Text.Encoding.UTF8.GetBytes(mensaje);
-                byte[] firma = rsa.SignData(mensajeBytes, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
-
-                // Guardar la firma en un archivo
-                File.WriteAllBytes("firma.txt", firma);
-
-                // Verificar la firma
-                bool verificado = rsa.VerifyData(mensajeBytes, firma, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
-                if (verificado)
-                {
-                    Console.WriteLine("La firma es válida.");
-                }
-                else
-                {
-                    Console.WriteLine("La firma es inválida.");
-                }
-            }
-
-
-
-        }
-        catch (CryptographicException e)
-        {
-            Console.WriteLine($"Error de criptografía: {e.Message}");
-        }
-    }
-
-    // Tip Método para convertir los parámetros RSA a XML
-    static string ToXmlString(RSAParameters rsaParameters)
-    {
-        using (var sw = new System.IO.StringWriter())
-        {
-            var xs = new System.Xml.Serialization.XmlSerializer(typeof(RSAParameters));
-            xs.Serialize(sw, rsaParameters);
-            return sw.ToString();
-        }
+        new MenuConsoleService(
+            new CryptoService(),
+            new RepositoryKeys( @"Data Source=./data/mydatabase.db;Version=3;"),
+            new FileManager("./files/")
+        );
     }
 }
